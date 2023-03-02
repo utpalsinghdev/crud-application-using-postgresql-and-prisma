d26c98b89abe71b07be7a27aa8c7b1ec7c033a4e
Utpal Singh <110197373+utpalsinghdev@users.noreply.github.com>
2023-03-02 17:45:22 +0530

    rate limiter added to application 10 req/ 5 sec

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,24 @@ import express, { NextFunction, Request, Response } from "express";
 import createHttpError, {isHttpError} from "http-errors";
 import morgan from "morgan";
 import testrou from "./test/test.routes";
+import rateLimit from 'express-rate-limit'
 const app = express();
 app.use(morgan("dev"))
 app.use(express.json());
 
 
-
+app.use(
+    rateLimit(
+        {
+            windowMs: 5000,
+            max: 10,
+            message: {
+                status: 429,
+                message: "Too many requests, please try again later"
+            }
+        }
+    )
+)
 app.use("/", testrou)
 
 app.use((req: Request, res: Response, next: NextFunction) => {
